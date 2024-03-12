216fa5043b79f8b677d1db078fc3af74d235f43f
Victor Lucas <victorlucas@MacBook-Air-de-Victor.local>
2024-03-12 15:41:38 -0400

    feat: animation loading

--- front/src/Components/Helper/Loading.jsx
+++ front/src/Components/Helper/Loading.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React from "react";
+import styles from "./Loading.module.css";
+import { Carregando } from "../../Assets/carregando";
 
-const Loading = () => {
-  return <div>Carregando...</div>;
-};
+export function Loading() {
+  const [step, setStep] = React.useState(1);
+
+  React.useEffect(() => {
+    function updateStep() {
+      setStep((step) => {
+        if (step < 3) return step + 1;
+        else return 0;
+      });
+    }
+
+    const interval = setInterval(updateStep, 300);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  function displayStep(i) {
+    return {
+      display: step === i ? "block" : "none",
+    };
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.loading}>
+        <Carregando displayStep={displayStep} />
+      </div>
+    </div>
+  );
+}
 
 export default Loading;
