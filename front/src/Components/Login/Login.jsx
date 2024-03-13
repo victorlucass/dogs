a51c9f11b41192dcd0a7a5c532956357a6e3d1b8
Victor Lucas <victorlucas@MacBook-Air-de-Victor.local>
2024-03-12 22:01:22 -0400

    feat: create LoginPasswodLost

--- front/src/Components/Login/Login.jsx
+++ front/src/Components/Login/Login.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import LoginForm from "./LoginForm";
 import LoginCreate from "./LoginCreate";
-import LoginPasswordLost from "./LoginPasswordLost";
+import { LoginPasswordLost } from "./LoginPasswordLost";
 import LoginPasswordReset from "./LoginPasswordReset";
 import { UserContext } from "../../UserContext";
 import styles from "./Login.module.css";
